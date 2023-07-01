4f3cabba1b6a4a99ffcf8700083a08e1114a2346
Palanath <pala@dusttoash.org>
2023-07-01 09:11:44 -0500

    Make really simple opengl window template

--- src/Template.cpp
+++ src/Template.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+#include <string>
 
 int main() {
+	if (!glfwInit()) {
+		std::cerr << "Error initializing GLFW." << std::endl;
+		return 0;
+	}
+
+	GLFWwindow *wind = glfwCreateWindow(1400, 800, "Window", nullptr, nullptr);
+	if (!wind) {
+		std::cerr << "Error creating window." << std::endl;
+		return 0;
+	}
+
+	while (!glfwWindowShouldClose(wind))
+		glfwPollEvents();
+
+	glfwDestroyWindow(wind);
+	glfwTerminate();
+
 	return 0;
 }
