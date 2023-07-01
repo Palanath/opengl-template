3a5a3cb599ed3678d820d0ff40dc00c0008c3f73
Palanath <pala@dusttoash.org>
2023-07-01 09:12:46 -0500

    Use macros for window properties

--- src/Template.cpp
+++ src/Template.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <string>
 
+#define WINDOW_WIDTH 1400
+#define WINDOW_HEIGHT 800
+#define WINDOW_TITLE "Window"
+
 int main() {
 	if (!glfwInit()) {
 		std::cerr << "Error initializing GLFW." << std::endl;
 		return 0;
 	}
 
-	GLFWwindow *wind = glfwCreateWindow(1400, 800, "Window", nullptr, nullptr);
+	GLFWwindow *wind = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+			WINDOW_TITLE, nullptr, nullptr);
 	if (!wind) {
 		std::cerr << "Error creating window." << std::endl;
 		return 0;
